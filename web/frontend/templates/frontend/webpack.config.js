07c9e8c07c7211508166e98709c61ed65ec46c9d
nikolaystanishev <stanishev.nikolay@gmail.com>
2022-03-10 09:29:02 +0200

    Move react app to frontend app of django

---
+++ web/frontend/templates/frontend/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        path: path.resolve(__dirname, "./static/frontend"),
+        filename: "[name].js"
+    },
+    mode: process.env.NODE_ENV || "development",
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    devServer: { contentBase: path.join(__dirname, "src") },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ["ts-loader"],
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+                use: ["file-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+              // This has effect on the react lib size
+              NODE_ENV: JSON.stringify("development"),
+            },
+          }),
+    ],
+};
