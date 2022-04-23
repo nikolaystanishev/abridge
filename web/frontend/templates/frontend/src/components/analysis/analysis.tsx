3a4604a297eee458da0f91e9108cfe6dc69425aa
nikolaystanishev <stanishev.nikolay@gmail.com>
2022-04-23 23:31:11 +0300

    Add summary to UI

--- web/frontend/templates/frontend/src/components/analysis/analysis.tsx
+++ web/frontend/templates/frontend/src/components/analysis/analysis.tsx
@@ -1,10 +1,10 @@
 import React, {useEffect} from "react";
 import {AnalysisT, DataObjectT, LabelT, PlatformsFilterT} from "../../types/types";
 import axios from "axios";
-import {Container, Grid} from "@mui/material";
+import {Card, CardContent, Container, Grid, Typography} from "@mui/material";
 import {TwitterTweetEmbed} from "react-twitter-embed";
-import PieChart, {Connector, Label, Series, Size,} from 'devextreme-react/pie-chart';
-import {Font} from "devextreme-react/chart";
+import {PieChart} from "devextreme-react";
+import {Connector, Font, Label, Series, Size, Title} from "devextreme-react/pie-chart";
 
 
 export function Analysis(props: { filter: PlatformsFilterT[], reload: boolean }) {
@@ -41,32 +41,50 @@ export function Analysis(props: { filter: PlatformsFilterT[], reload: boolean })
             })}
           </Grid>
           <Grid item xs>
-            <PieChart
-              dataSource={Object.values(analysis.data.map((item: DataObjectT) => item.label == LabelT.POSITIVE ? 'Positive' : 'Negative').reduce((data: { [key: string]: { label: string, value: number } }, label: string) => {
-                data[label].value += 1;
-                return data;
-              }, {'Positive': {label: 'Positive', value: 0}, 'Negative': {label: 'Negative', value: 0}}))}
-              title="Distribution of Public Opinion"
-              customizePoint={(point: any) => {
-                return {
-                  color: point.argument == 'Positive' ? '#4caf50' : '#ef5350',
-                };
-              }}
-            >
-              <Series
-                argumentField="label"
-                valueField="value">
-                <Label
-                  visible={true}
-                  position="columns"
-                  customizeText={(arg: any) => `${arg.valueText} (${arg.percentText})`}>
-                  <Font size={16}/>
-                  <Connector visible={true} width={0.5}/>
-                </Label>
-              </Series>
-
-              <Size width={500}/>
-            </PieChart>
+            <Grid container item spacing={3} direction='column'
+                  alignItems="baseline" margin='auto' width='80%'>
+              <Grid item xs>
+                <PieChart
+                  dataSource={Object.values(analysis.data.map((item: DataObjectT) => item.label == LabelT.POSITIVE ? 'Positive' : 'Negative').reduce((data: { [key: string]: { label: string, value: number } }, label: string) => {
+                    data[label].value += 1;
+                    return data;
+                  }, {'Positive': {label: 'Positive', value: 0}, 'Negative': {label: 'Negative', value: 0}}))}
+                  customizePoint={(point: any) => {
+                    return {
+                      color: point.argument == 'Positive' ? '#4caf50' : '#ef5350',
+                    };
+                  }}
+                >
+                  <Title text='Distribution of Public Opinion' horizontalAlignment='center'>
+                    <Font family='' weight='400' size='1.5rem' color='#000000DE'/>
+                  </Title>
+                  <Series
+                    argumentField="label"
+                    valueField="value">
+                    <Label
+                      visible={true}
+                      position="columns"
+                      customizeText={(arg: any) => `${arg.valueText} (${arg.percentText})`}>
+                      <Font size={16}/>
+                      <Connector visible={true} width={0.5}/>
+                    </Label>
+                  </Series>
+                  <Size width={500}/>
+                </PieChart>
+              </Grid>
+              <Grid item xs>
+                <Card variant="elevation" elevation={0}>
+                  <CardContent>
+                    <Typography variant="h5" component="div">
+                      Summary of Public Opinion
+                    </Typography>
+                    <Typography variant="body2" textAlign='justify'>
+                      {analysis.summary}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       )}
