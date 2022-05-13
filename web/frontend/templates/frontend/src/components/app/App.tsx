dacbbfeeb9298de5e43f00b168efb2c026487a6a
nikolaystanishev <stanishev.nikolay@gmail.com>
2022-05-14 00:26:14 +0300

    Change color theme with theme provider

--- web/frontend/templates/frontend/src/components/app/App.tsx
+++ web/frontend/templates/frontend/src/components/app/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
 import {Report} from "../report/report";
+import {Header} from "./header";
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#c6f1e7'
+    },
+    secondary: {
+      main: '#59606d'
+    }
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <Report/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Header/>
+        <Report/>
+      </div>
+    </ThemeProvider>
   );
 }
 
