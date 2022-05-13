042e25b0c555f0300b2bfc23f3ae385c79784457
nikolaystanishev <stanishev.nikolay@gmail.com>
2022-05-14 00:17:39 +0300

    Add favicon

--- web/web/urls.py
+++ web/web/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'))),
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
     path('api/', include('analysis_api.urls')),
